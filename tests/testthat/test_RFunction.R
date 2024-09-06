9c98485e52bd8520f6347f7d5f1ef0d537b088d0
alerte-telemetrie <alerte.telemetrie@lpo.fr>
2024-09-06 14:04:54 +0200

    Update test_RFunction.R

--- tests/testthat/test_RFunction.R
+++ tests/testthat/test_RFunction.R
@@ -1,4 +1,5 @@
-test_data <- test_data("input3_move2loc_LatLon.rds")
+#test_data <- test_data("input3_move2loc_LatLon.rds")
+test_data <- test_data("data_test_SelectIndividual-byBatteryCharge.rds")
 
 test_that("happy path", {
   actual <- rFunction(data = test_data, sdk = "unit test", year = 1998)
